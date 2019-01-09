e1565668bc10dc0e8c2810d484fa8d80bac0e027
mohamed-saad <mahameds19999@gmail.com>
2019-01-09 14:55:29 +0200

    secure the keys adding these keys in separate file and add this file to gitignore and import the file in the passport config file

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,11 @@
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20');
+const keys = require('./keys');
 
 passport.use(
     new googleStrategy({
-        clientID: '893086344264-6gll932t5qcuafmglq6m4ntsf03k1htf.apps.googleusercontent.com',
-        clientSecret: 'dpBbWaXXzD2FtGyz8WDg5IFR'
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
     }), () => {
 
     }
