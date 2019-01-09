8caab505d05d6f09b42867131646409cd248a800
mohamed-saad <mahameds19999@gmail.com>
2019-01-09 18:32:39 +0200

    serializing user & deserializing

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,16 @@ const googleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
 const User = require('../models/user-model');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user, done) => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new googleStrategy({
         callbackURL: '/auth/google/redirect',
@@ -13,12 +23,14 @@ passport.use(
         User.findOne({googleID: profile.id}).then((user) => {
             if(user) {
                 console.log('the current user: ' + user);
+                done(null, user);
             } else {
                 new User({
                     username: profile.displayName,
                     googleID: profile.id
                 }).save().then((newUser) => {
                     console.log(newUser);
+                    done(null, newUser);
                 })
             }
         })
