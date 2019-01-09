a0e4fc31c72636d80bfd0518e5898962dece35da
mohamed-saad <mahameds19999@gmail.com>
2019-01-09 14:48:50 +0200

    setup the google+ API

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,8 +3,9 @@ const googleStrategy = require('passport-google-oauth20');
 
 passport.use(
     new googleStrategy({
-
+        clientID: '893086344264-6gll932t5qcuafmglq6m4ntsf03k1htf.apps.googleusercontent.com',
+        clientSecret: 'dpBbWaXXzD2FtGyz8WDg5IFR'
     }), () => {
-        
+
     }
 )
